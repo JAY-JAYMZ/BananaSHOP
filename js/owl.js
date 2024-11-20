2891d5a0ba1e2864f63060ccb953ca2b9efb3c7c
JAY-JAYMZ <164818244+JAY-JAYMZ@users.noreply.github.com>
2024-11-20 20:00:24 +0800

    Update owl.js

--- js/owl.js
+++ js/owl.js
@@ -1,20 +1,6 @@
-/**
- * Owl Carousel v2.3.4
- * Copyright 2013-2018 David Deutsch
- * Licensed under: SEE LICENSE IN https://github.com/OwlCarousel2/OwlCarousel2/blob/master/LICENSE
- */
-/**
- * Owl carousel
- * @version 2.3.4
- * @author Bartosz Wojciechowski
- * @author David Deutsch
- * @license The MIT License (MIT)
- * @todo Lazy Load Icon
- * @todo prevent animationend bubling
- * @todo itemsScaleUp
- * @todo Test Zepto
- * @todo stagePadding calculate wrong active classes
- */
+/*!
+223190722 JAY JAYMZ  Wu Jing 
+*/
 ;(function($, window, document, undefined) {
 
 	/**
